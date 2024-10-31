f22ffb26d56d9ee5f245f6939ad5bec16ee08b1f
sobhanzadehali <99137872+sobhanzadehali@users.noreply.github.com.>
2024-10-31 20:23:39 +0330

    list-create tag apiview

--- core/tagger/urls.py
+++ core/tagger/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('dataset/<int:dataset_id>/<int:tag_id>/', views.SentenceCategory.as_view(), name='category'),
-    
+    # list and create tag
+    path('dataset/<int:dataset_id>/tags/', views.TagAPIView.as_view(), name='tag-list'),
+    # lists sentences tagged with specific tag
+    path('dataset/<int:dataset_id>/<int:tag_id>/', views.SentenceCategoryAPIView.as_view(), name='category'),
+
 ]
--- core/tagger/views.py
+++ core/tagger/views.py
@@ -1,16 +1,17 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
 
 from .models import Dataset, Operator, HasPermission, Tag, Sentence, LabeledSentence
-from .serializers import LabeledSentenceSerializer, SentenceSerializer
+from .serializers import LabeledSentenceSerializer, SentenceSerializer, TagSerializer
 
 
 # Create your views here.
 
-class SentenceCategory(APIView):
+
+class SentenceCategoryAPIView(APIView):
     serializer_class = LabeledSentenceSerializer
     permission_classes = (IsAuthenticated,)
 
@@ -32,3 +33,32 @@ class SentenceCategory(APIView):
 
         return Response(status=status.HTTP_403_FORBIDDEN)
 
+
+class TagAPIView(APIView):
+    serializer_class = TagSerializer
+    permission_classes = (IsAdminUser,)
+
+    def get(self, request, dataset_id):
+        """
+        list all tages in a dataset if you have permission.
+        """
+        user = request.user
+        operator = Operator.objects.get(user=user)
+        try:
+            is_allowed = HasPermission.objects.get(operator=operator, dataset__pk=dataset_id)
+        except HasPermission.DoesNotExist as e:
+            return Response({"detail": "you don't have permission"}, status=status.HTTP_400_BAD_REQUEST)
+        tags = Tag.objects.filter(dataset__pk=dataset_id, is_active=True)
+        serializer = self.serializer_class(tags, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, dataset_id):
+        """
+        create tag if you have permission.
+        """
+        user = request.user
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
