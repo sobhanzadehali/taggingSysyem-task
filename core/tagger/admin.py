f91bc28bbbb44ca6e6961f75c5a87e1c2def036a
sobhanzadehali <99137872+sobhanzadehali@users.noreply.github.com.>
2024-10-31 16:48:42 +0330

    add-models

--- core/tagger/admin.py
+++ core/tagger/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dataset, Tag, Operator, Sentence, HasPermission
 
 # Register your models here.
+admin.site.register(Dataset)
+admin.site.register(Tag)
+admin.site.register(HasPermission)
+admin.site.register(Operator)
+admin.site.register(Sentence)
--- core/tagger/models.py
+++ core/tagger/models.py
@@ -1,3 +1,56 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+class Dataset(models.Model):
+    name = models.CharField(_("name"), max_length=255)
+    description = models.TextField(_("description"))
+
+    def __str__(self):
+        return self.name
+
+
+class Operator(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.user.username
+    
+
+class HasPermission(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    operator = models.ForeignKey(Operator, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "permission"
+        verbose_name_plural = "permissions"
+
+    def __str__(self):
+        return f"{self.dataset.name} - {self.operator.user.username}"
+
+
+class Tag(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    name = models.CharField(_("name"), max_length=255)
+    is_active = models.BooleanField(_("is_active"), default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Sentence(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, blank=True, null=True)
+    body = models.TextField(_("body"))
+
+    def __str__(self):
+
+        return f"{self.body[:50]}..."
+
+
+class LabeledSentence(models.Model):
+    sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.sentence[:50]}, {self.tag[:50]}"
